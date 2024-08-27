25efd639d4f5fb053343bba4de5d6eb2d80b3714
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2024-08-27 15:50:19 +0100

    Render all reports

--- docker/examples.sh
+++ docker/examples.sh
@@ -6,4 +6,5 @@ docker container run \
   --mount type=bind,source="/Volumes/igmm/cvallejo-predicct/libdr/",target="/analysis/data" \
   --mount type=bind,source="$(pwd)/docs",target="/analysis/docs" \
   --mount type=bind,source="$(pwd)",target="/analysis/src" \
+  --rm \
   ghcr.io/nathansam/libdr
