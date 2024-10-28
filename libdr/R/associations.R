95b8afb4489dcb3c5181bc37e6fc683ad69c76e3
Catalina Vallejos <catalina.vallejos@ed.ac.uk>
2024-10-28 11:12:54 +0000

    extra plots for associations FC 8 clusters

--- libdr/R/associations.R
+++ libdr/R/associations.R
@@ -8,8 +8,8 @@
 plotCat <- function(dat, var, class = "class_combined") {
   cluster <- NULL
 
-  fill.vec <- c("#C2F8CB", "#EFC7E5", "#F6BD60")
-  col.vec <- c("#82C68F", "#C795BB", "#C89216")
+  fill.vec <- c("#C2F8CB", "#EFC7E5", "#F6BD60", "blue")
+  col.vec <- c("#82C68F", "#C795BB", "#C89216", "blue")
 
   if (!(var %in% colnames(dat))) stop("Column for categorical variable not found.")
   if (!(class %in% colnames(dat))) stop("Class column not found.")
@@ -79,6 +79,54 @@ plotCat <- function(dat, var, class = "class_combined") {
   return(p)
 }
 
+.getCI <- function(m) {
+
+  CI_factor <- qnorm(0.975)
+
+  # A chatGPT edited version of the original code :-)
+  m_summary <- summary(m)
+  m_coeff <- as.data.frame(m_summary$coefficients) %>%
+    rownames_to_column("Var1") %>%
+    pivot_longer(-Var1, names_to = "Var2", values_to = "Estimate") %>%
+    mutate(var = paste(Var1, Var2))
+  m_se <- as.data.frame(m_summary$standard.errors)  %>%
+    rownames_to_column("Var1") %>%
+    pivot_longer(-Var1, names_to = "Var2", values_to = "SE") %>%
+    mutate(var = paste(Var1, Var2)) %>%
+    select(c(var, SE))
+  m_coeff <- merge(m_coeff, m_se, by = "var") %>%
+    mutate(z = Estimate/SE,
+           p.val = (1 - pnorm(abs(z), 0, 1)) * 2,
+           Lower = Estimate - CI_factor * SE,
+           Upper = Estimate + CI_factor * SE,
+           Sig = ifelse(Upper < 0 | Lower > 0, TRUE, FALSE)) %>%
+    arrange(Var2, Var1) %>%
+    subset(Var2 != "(Intercept)")
+
+  return(m_coeff)
+}
+
+.plotCI <- function(tab, variable) {
+
+  tab %>%
+    filter(Var2 == variable) %>%
+    ggplot(aes(
+      x = Var1,
+      y = Estimate,
+      ymin = Lower,
+      ymax = Upper,
+      color = ifelse(Sig == TRUE, "red", "black"))) +
+    geom_errorbar() +
+    geom_point(size = 3.5) +
+    geom_hline(yintercept = 0, lty = 2) +
+    coord_flip() + # flip coordinates (puts labels on y axis)
+    xlab("") +
+    ylab("Estimate (95% CI)") +
+    theme_bw() +
+    scale_color_manual(values = c("black", "#FF007F")) +
+    theme(legend.position = "none") +
+    ggtitle(variable)
+}
 
 #' Forest plot of multinomial logistic regression model
 #' @param dat  Data frame holding covariate and cluster assignment data.
@@ -90,61 +138,43 @@ plotCat <- function(dat, var, class = "class_combined") {
 #' @export
 mlrPlot <- function(dat, var, class = "class_combined") {
 
-  Estimate <- Lower <- Upper <- Var1 <- Var2 <- sig <- NULL
-
-  results <- list()
-  mlr <- nnet::multinom(formula = reformulate(var, class), data = dat, trace = FALSE)
-  z <- summary(mlr)$coefficients / summary(mlr)$standard.errors
-  p.val <- (1 - pnorm(abs(z), 0, 1)) * 2
-
-  coeff <- summary(mlr)$coefficients
-  coeff <- reshape2::melt(coeff)
-  coeff$var <- with(coeff, paste(Var1, Var2))
-  coeff <- filter(coeff, Var2 != "(Intercept)")
-  coeff <- coeff
-  colnames(coeff) <- c("Var1", "Var2", "Estimate", "var")
-  lower <- summary(mlr)$coefficients - (qnorm(0.975) * summary(mlr)$standard.errors)
-  lower <- reshape2::melt(lower)
-  lower$var <- with(lower, paste(Var1, Var2))
-  lower <- filter(lower, Var2 != "(Intercept)")
-  lower <- lower %>%
-    select(-Var1, -Var2)
-  colnames(lower) <- c("Lower", "var")
-  tab <- merge(coeff, lower, by = "var")
-  upper <- summary(mlr)$coefficients + (qnorm(0.975) * summary(mlr)$standard.errors)
-  upper <- reshape2::melt(upper)
-  upper$var <- with(upper, paste(Var1, Var2))
-  upper <- filter(upper, Var2 != "(Intercept)")
-  upper <- upper %>%
-    select(-Var1, -Var2)
-  colnames(upper) <- c("Upper", "var")
-  tab <- merge(tab, upper, by = "var")
-  tab$var <- factor(tab$var, levels = rev(tab$var))
-  tab$sig <- FALSE
-  tab[tab$Upper < 0, "sig"] <- TRUE
-  tab[tab$Lower > 0, "sig"] <- TRUE
-  p <- list()
-
-  for (variable in unique(tab$Var2)) {
-    p[[variable]] <- tab %>%
-      filter(Var2 == variable) %>%
-      ggplot(aes(
-        x = Var1,
-        y = Estimate,
-        ymin = Lower,
-        ymax = Upper,
-        color = ifelse(sig == TRUE, "red", "black"))) +
-      geom_errorbar() +
-      geom_point(size = 3.5) +
-      geom_hline(yintercept = 0, lty = 2) +
-      coord_flip() + # flip coordinates (puts labels on y axis)
-      xlab("") +
-      ylab("Estimate (95% CI)") +
-      theme_bw() +
-      scale_color_manual(values = c("black", "#FF007F")) +
-      theme(legend.position = "none") +
-      ggtitle(variable)
-    #print(p)
+  # Multivariate analysis
+  ## Fit the model
+  mlr_multi <- nnet::multinom(formula = reformulate(var, class), data = dat, trace = FALSE)
+  ## Extract coeffs and CIs
+  tab_multi <- .getCI(mlr_multi)
+  ## Plot
+  p_multi <- list()
+  for (variable in unique(tab_multi$Var2)) {
+    p_multi[[variable]] <- .plotCI(tab_multi, variable)
   }
-  list(coeff = summary(mlr)$coefficients , z = z, p.val = p.val, plot = p)
+
+  # Univariate analysis
+  if(length(var) > 1) {
+
+    ## Fit the model + Extract coeffs and CIs
+    tab_uni <- NULL
+    for (variable in var) {
+      mlr_uni <- nnet::multinom(formula = reformulate(variable, class),
+                                data = dat, trace = FALSE)
+      tab_uni <- rbind(tab_uni, .getCI(mlr_uni))
+    }
+    ## Plot
+    p_uni <- list()
+    for (variable in unique(tab_uni$Var2)) {
+      p_uni[[variable]] <- .plotCI(tab_uni, variable)
+    }
+  }
+
+  # Output
+  if(length(var) == 1) {
+    out <- list(tab_multi = tab_multi,
+                plot_multi = p_multi)
+  } else {
+    out <- list(tab_multi = tab_multi,
+                plot_multi = p_multi,
+                tab_uni = tab_uni,
+                plot_uni = p_uni)
+  }
+  return(out)
 }
